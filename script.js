0086a722b9185780579d8941085ef504e6b43f5e
ericsonvaz-Dev <ericson.vaz@gmail.com>
2025-10-18 11:19:15 -0300

    ajustando o JavaScript

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ function generateNumber() {
     const min = Math.ceil(document.getElementById('minimo').value)
     const max = Math.floor(document.getElementById('maximo').value)
     
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert('O valor mínimo deve ser MENOR que o valor máximo.')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
     const sorteado = document.getElementById('resultado')
     
 
     sorteado.innerHTML = `O número sorteado foi: <strong>${result}</strong>`
-    
-    
+    }
 }
